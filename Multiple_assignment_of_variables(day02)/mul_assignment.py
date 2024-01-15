5c6a6b840fb8983a72826142e1ddd8aa708d79c1
AdityaRoy999 <adiroyboy2@gmail.com>
2024-01-16 00:23:38 +0530

    Updated

---
+++ Multiple_assignment_of_variables(day02)/mul_assignment.py
@@ -0,0 +1,8 @@
+#Multiple assignment = assign multiple variables at the same time.
+name ,age ,height = "aditya" , 5 , 250.5
+print(name,age,height)
+print(type(name),type(age),type(height))
+
+num1 = num2 = num3 = 67
+print(num1)
+print(type(num1))
---
+++ Variables(day01)/variables.py
@@ -0,0 +1,23 @@
+#type command prints the data type 
+#strings 
+first_name = "Aditya"
+last_name= "Roy"
+full_name = first_name +last_name
+print(full_name)
+print(type(full_name))
+
+#integer
+age = 21
+age +=1
+print("your age is "+str(age))
+print(type(age))
+
+#float
+height = 250.5
+print("your height is" +str(height))
+print(type(height))
+
+#boolean
+human = True
+print(human)
+print(type(human))
